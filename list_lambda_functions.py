ef1c7967649db21ac9071ff4dbd2189ae9a14ef6
Conor <37616617+Conor-Ryan8@users.noreply.github.com>
2023-06-19 00:07:22 +0100

    checking for versions and grouping them together
    checking for versions and grouping them together

--- list_lambda_functions.py
+++ list_lambda_functions.py
@@ -2,17 +2,22 @@ import boto3
 
 lambdas = boto3.client('lambda', region_name='eu-north-1')
 
-function_list = []
+function_list = {}
 
 def lambda_handler(event, context):
-    
+
     lambda_list = lambdas.list_functions(FunctionVersion='ALL', MaxItems=123)
-    
+
     for function in lambda_list['Functions']:
+
         name = function['FunctionName']
         codesize = str(function['CodeSize']) + " bytes"
         memsize = str(function['MemorySize']) + "MB"
         version = function['Version']
-        function_list.append([name, codesize, memsize, version])
-        
-    return function_list
+
+        if name in function_list.keys():
+            function_list[name].append({'Version:' : version, 'CodeSize:' : codesize, 'MemorySize:' : memsize})
+        else:
+            function_list[name] = [{'Version:' : version, 'CodeSize:' : codesize, 'MemorySize:' : memsize}]
+
+    return {'statusCode' : 200, 'function list' : function_list}
