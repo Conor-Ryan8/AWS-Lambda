baa4074bf904e3c8737a0b900ada5886827c1d66
Conor <37616617+Conor-Ryan8@users.noreply.github.com>
2023-06-18 22:51:01 +0100

    Update list_lambda_functions.py

--- list_lambda_functions.py
+++ list_lambda_functions.py
@@ -1,9 +1,18 @@
 import boto3
 
-cloudwatch = boto3.client('cloudwatch', region_name='eu-north-1')
 lambdas = boto3.client('lambda', region_name='eu-north-1')
 
+function_list = []
+
 def lambda_handler(event, context):
+    
     lambda_list = lambdas.list_functions(FunctionVersion='ALL', MaxItems=123)
-    print(lambda_list)
-    return lambda_list
+    
+    for function in lambda_list['Functions']:
+        name = function['FunctionName']
+        codesize = str(function['CodeSize']) + " bytes"
+        memsize = str(function['MemorySize']) + "MB"
+        version = function['Version']
+        function_list.append([name, codesize, memsize, version])
+        
+    return function_list
