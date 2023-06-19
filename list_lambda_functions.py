8357defcd8ddf1d6c6b911162c7b5b40f97df688
Conor <37616617+Conor-Ryan8@users.noreply.github.com>
2023-06-19 10:38:55 +0100

    added total size, alert status and comments

--- list_lambda_functions.py
+++ list_lambda_functions.py
@@ -1,23 +1,47 @@
 import boto3
 
-lambdas = boto3.client('lambda', region_name='eu-north-1')
-
-function_list = {}
+lambdas = boto3.client('lambda')
 
 def lambda_handler(event, context):
-
-    lambda_list = lambdas.list_functions(FunctionVersion='ALL', MaxItems=123)
-
-    for function in lambda_list['Functions']:
-
+    
+    functions = {}
+    size = 0
+    
+    #Get a list of all Lambda functions and all versions
+    lambdalist = lambdas.list_functions(FunctionVersion='ALL', MaxItems=9999)
+    
+    #Iterate through the list of functions
+    for function in lambdalist['Functions']:
+        
+        #Extract the function name
         name = function['FunctionName']
-        codesize = str(function['CodeSize']) + " bytes"
-        memsize = str(function['MemorySize']) + "MB"
+        
+        #Extract the function version
         version = function['Version']
+        
+        #Extract the code size
+        codesize = str(function['CodeSize']) + " bytes"
 
-        if name in function_list.keys():
-            function_list[name].append({'Version:' : version, 'CodeSize:' : codesize, 'MemorySize:' : memsize})
+        #calculate the total code size
+        size = size + function['CodeSize']
+
+        #Check if function is already in payload
+        if name in functions.keys():
+            
+            #If it is then add a new version for this function
+            functions[name].append({'Version:' : version, 'CodeSize:' : codesize})
         else:
-            function_list[name] = [{'Version:' : version, 'CodeSize:' : codesize, 'MemorySize:' : memsize}]
+            #If it is not then add this version to the payload
+            functions[name] = [{'Version:' : version, 'CodeSize:' : codesize}]
 
-    return {'statusCode' : 200, 'function list' : function_list}
+    #Add the total code size to the payload
+    functions['TotalSize'] = str(size) + " bytes"
+
+    #Check if the total code size exceeds 90% of 75GB
+    if size >= 72477573120:
+        functions['Alert'] = "YES"
+    else:
+        functions['Alert'] = "NO"
+    
+    #Return the payload
+    return {'statusCode' : 200, 'function list' : functions}
